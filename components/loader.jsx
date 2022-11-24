ac07ac5e72f3ecb22720e8650e744fa6119798ee
iddrismystic <abdulwahabiddris08@gmail.com>
2022-11-24 19:52:48 +0000

    i

--- components/loader.jsx
+++ components/loader.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Loader() {
   return (
-    <div className='loadContainer'><div class="loading" data-loading-text="Loading"></div></div>
+    <div className='loadContainer'><div class="loading"></div></div>
   )
 }

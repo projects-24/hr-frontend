8b7f18658ad3885a277827b80e1bac33579900b6
iddrismystic <funtechs45@gmail.com>
2023-09-25 02:18:31 +0100

    i

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }) {
    <link rel="preconnect" href="https://fonts.gstatic.com" />
    <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet" />
    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.csss" rel="stylesheet" />
-   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400;500;600;700;800;900&display=swap" />
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
     </Head>
 <div className="body">
 <Component {...pageProps} />
